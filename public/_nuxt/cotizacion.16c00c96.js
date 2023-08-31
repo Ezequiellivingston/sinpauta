a18a07e90e23c22214e19c5933c1b965e34d81da
plivingston <plivingston@sofredigital.com.ar>
2023-08-30 21:07:58 -0300

    new notice

---
+++ public/_nuxt/cotizacion.16c00c96.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./entry.fb2c0e53.js";const r={};function o(t,e){return" dolar "}const _=c(r,[["render",o]]);export{_ as default};
--- public/_nuxt/cotizacion.2235bd94.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./entry.201b9195.js";const r={};function o(t,e){return" dolar "}const _=c(r,[["render",o]]);export{_ as default};

2d41746679bd2c6dedaa30127c8d2ce7fea49366
plivingston <plivingston@sofredigital.com.ar>
2023-09-01 15:52:22 -0300

    new notice

--- public/_nuxt/mp-logo.396f50f8.js
+++
@@ -1,1 +0,0 @@
-import"./entry.f927eb8c.js";const s=""+globalThis.__publicAssetsURL("mp-logo.png");export{s as _};
---
+++ public/_nuxt/mp-logo.4ddcd5a6.js
@@ -0,0 +1,1 @@
+import"./entry.95cc5449.js";const s=""+globalThis.__publicAssetsURL("mp-logo.png");export{s as _};

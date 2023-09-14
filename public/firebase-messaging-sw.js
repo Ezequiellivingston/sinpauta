7fde25ea568309cd5cfec7a12c528d014c8846ac
plivingston <plivingston@sofredigital.com.ar>
2023-09-13 22:38:56 -0300

    new notice

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,33 +1,45 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js"
+);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCihtmlyKeFa6MGXMrJ3DPUAm7s8k-TJyo",
-    authDomain: "autocompromiso-32ca7.firebaseapp.com",
-    databaseURL: "https://autocompromiso-32ca7-default-rtdb.firebaseio.com",
-    projectId: "autocompromiso-32ca7",
-    storageBucket: "autocompromiso-32ca7.appspot.com",
-    messagingSenderId: "171009053819",
-    appId: "1:171009053819:web:8fd75b1bdbde7a08eabede",
-    measurementId: "G-E5P4RN1V8K",
-  };
+  apiKey: "AIzaSyCihtmlyKeFa6MGXMrJ3DPUAm7s8k-TJyo",
+  authDomain: "autocompromiso-32ca7.firebaseapp.com",
+  databaseURL: "https://autocompromiso-32ca7-default-rtdb.firebaseio.com",
+  projectId: "autocompromiso-32ca7",
+  storageBucket: "autocompromiso-32ca7.appspot.com",
+  messagingSenderId: "171009053819",
+  appId: "1:171009053819:web:8fd75b1bdbde7a08eabede",
+  measurementId: "G-E5P4RN1V8K",
+};
 
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(payload, 'asdasd')
+messaging.onBackgroundMessage(payload => {
+  console.log(payload, "asdasd");
   const options = {
     body: payload.notification.body,
-    icon: '/sinpauta.png',
+    icon: "/sinpauta.png",
     badge: payload.notification.image,
     click_action: payload.data.url,
     data: {
       url: payload.data.url,
     },
   };
-  
-  return  self.registration.showNotification(payload.notification
-    .title, options)
-})
+
+  const url = payload.data.url;
+
+  if (url) {
+    clients.openWindow(url);
+  }
+
+  return self.registration.showNotification(
+    payload.notification.title,
+    options
+  );
+});

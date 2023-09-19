f69ff1564c489a032663f8a955e403a1b15b4cc6
plivingston <plivingston@sofredigital.com.ar>
2023-09-19 15:22:41 -0300

    new notice

---
+++ public/_nuxt/test.082ffb2d.js
@@ -0,0 +1,1 @@
+import{r as a,o,l as n,k as t}from"./entry.dddaea11.js";const l={__name:"test",setup(r){const e=a();return o(()=>{e.value=navigator.language||navigator.userLanguage,console.log(e.value)}),(s,u)=>(t(),n("div"))}};export{l as default};

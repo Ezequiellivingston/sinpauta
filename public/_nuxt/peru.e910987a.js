566fc6c6fd16bb69444c40363b0754ef9233413b
plivingston <plivingston@sofredigital.com.ar>
2023-09-21 10:55:53 -0300

    new notice

---
+++ public/_nuxt/peru.e910987a.js
@@ -0,0 +1,16 @@
+import{H as y,M as v,T as N}from"./components.459ad74f.js";import{u as P,V as g,N as x}from"./Noticia.be91ceb2.js";import{_ as S,L as T,l as p,q as e,s as t,F as m,r as k,H as b,O as D,P as V,Q as E,X as F,M as H,N as I,k as l,W as L,J as M,v as q,K as B,m as f,x as R,y as C}from"./entry.b5a4fca5.js";import{k as _,l as d}from"./VRow.2a79e2d9.js";const $=n=>(R("data-v-32432b53"),n=n(),C(),n),A=$(()=>f("h1",{class:"title",alt:"Perú al Día: Noticias de Última Hora"}," Perú al Día: Noticias de Última Hora ",-1)),U={__name:"peru",setup(n){const h=T(),{data:w}=P(async()=>{const c=k([]);let u=b();const a=D(u,"noticias"),i=await V(E(a,F("pais","==","Peru")));for(const o of i.docs){const s=H(h,`5ETmTP49k/${o.id}.jpeg`),r=await I(s);c.value.push({url:r,id:o.id,...o.data()})}return{noticias:c}},"$Lzw6sPcLY8");return(c,u)=>{const a=v,i=N,o=y;return l(),p(m,null,[e(_,{class:"container-new pa-2"},{default:t(()=>[e(d,{cols:"12",md:"9"},{default:t(()=>[A,e(g,{class:"pb-4"}),e(_,null,{default:t(()=>{var s;return[(l(!0),p(m,null,L((s=M(w))==null?void 0:s.noticias,(r,j)=>(l(),B(d,{cols:"12",sm:"12",key:r.id},{default:t(()=>[f("article",null,[e(x,{noticia:r},null,8,["noticia"])])]),_:2},1024))),128))]}),_:1})]),_:1}),e(d,{md:"4",class:"anuncios text-center"})]),_:1}),e(o,null,{default:t(()=>[e(a,{charset:"UTF-8"}),e(a,{name:"viewport",content:"width=device-width, initial-scale=1.0"}),e(a,{name:"description",content:"Sinpauta: Explora la actualidad de Perú y del mundo en nuestro portal de noticias. Mantente informado sobre política, economía, sociedad, cultura, deportes, salud, tecnología y entretenimiento. Los titulares más relevantes y la información de última hora, todo en un solo lugar."}),e(a,{name:"keywords",content:`Peru\r
+        Perú\r
+Noticias\r
+Actualidad\r
+Titulares\r
+Información\r
+Última hora\r
+Política\r
+Economía\r
+Sociedad\r
+Cultura\r
+Deportes\r
+Internacional\r
+Salud\r
+Tecnología\r
+Entretenimiento`}),e(a,{name:"author",content:"Equipo de Sinpauta"}),e(i,null,{default:t(()=>[q("Sinpauta Perú (pe) - Noticias y Novedades")]),_:1})]),_:1})],64)}}},Q=S(U,[["__scopeId","data-v-32432b53"]]);export{Q as default};
